149f9d4badab2da921cdc20649ecb5070eed73d0
mnindrazaka <mnindrazaka@gmail.com>
2018-02-27 20:24:14 +0700

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from '../components/MyLayout'
+
+export default () => (
+  <div>
+    <Layout>
+      <p>this is the about page</p>
+    </Layout>
+  </div>
+)
